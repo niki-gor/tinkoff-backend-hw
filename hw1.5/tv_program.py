ed1f2e1b9ac685f1c1e75145bf8b7ad73d519a4a
niki-gor <nic.gor@yandex.ru>
2022-10-10 01:15:55 +0300

    Get argv params when execute module

--- hw1.5/tv_program.py
+++ hw1.5/tv_program.py
@@ -1,3 +1,11 @@
-if __name__ == "__main__" or __name__ == 'tv_program':
-    # TODO write here
-    print("test")
+import sys
+import requests
+from pydantic import BaseModel
+
+
+API_URL = 'https://www.tvmaze.com/api#show-single-search'
+
+
+if __name__ == '__main__' or __name__ == 'tv_program':
+    response = requests.get(API_URL)
+    print(response.content)
